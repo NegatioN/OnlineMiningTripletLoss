c149f493c877dcfef9a6b7b1cccde39eaa633c0a
f-fl0 <66578286+f-fl0@users.noreply.github.com>
2021-01-24 08:00:42 +0000

    Update losses.py from the new notebook

--- online_triplet_loss/losses.py
+++ online_triplet_loss/losses.py
@@ -192,6 +192,5 @@ def batch_all_triplet_loss(labels, embeddings, margin, squared=False):
 
     # Get final mean triplet loss over the positive valid triplets
     triplet_loss = triplet_loss.sum() / (num_positive_triplets + 1e-16)
-    print(triplet_loss, fraction_positive_triplets)
 
     return triplet_loss, fraction_positive_triplets
